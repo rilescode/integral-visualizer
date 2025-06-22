37bff6632f2afc65b0c3fef92786abe41292c2e4
Riley C <rileycrahen@gmail.com>
2025-06-21 22:46:35 -0700

    make sure src settings in sys path

--- src/trydjango/wsgi.py
+++ src/trydjango/wsgi.py
@@ -10,17 +10,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 import sys
 
-# Add the src directory to Python path so trydjango module can be found
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.dirname(current_dir)  # Go up one level to src/
-project_root = os.path.dirname(src_dir)  # Go up one more level to project root
-
-# Add both src and project root to Python path
-sys.path.insert(0, src_dir)
-sys.path.insert(0, project_root)
+# Add the project root (containing 'src') to sys.path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+src_path = os.path.join(project_root, 'src')
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango.settings")
-
 application = get_wsgi_application()
